2f27a7fbddd1ff537eed37e200932868192b0dcf
jshields91 <jeshie02@louisville.edu>
2015-02-11 10:17:32 -0500

    changes

--- js/app.js
+++ js/app.js
@@ -1,22 +1,12 @@
 (function() {
-  var app = angular.module('gemStore', ['store-directives']);
+  var app = angular.module('listJson', []);
 
-  app.controller('StoreController', ['$http', function($http){
-    var store = this;
-    store.products = [];
-    $http.get('/store-products.json').success(function(data){
-    store.products = data;
+  app.controller('ListController', ['$http', function($http){
+    var list = this;
+    list.items = [];
+    $http.get("/tutorials.json").success(function(data){
+    list.items = data;
     });
     
   }]);
-
-  app.controller('ReviewController', function() {
-    this.review = {};
-
-    this.addReview = function(product) {
-      product.reviews.push(this.review);
-
-      this.review = {};
-    };
-  });
 })();
